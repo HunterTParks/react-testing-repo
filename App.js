05c5ae8c2ceedceb6ccb2c542971cc4a7cfbd478
Hunter Parks <hunter.thomas.parks@gmail.com>
2018-09-29 10:40:33 -0700

    add image to app.js

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    }
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Image source={pic} style={{width: 193, height: 110}}/>
       </View>
     );
   }
